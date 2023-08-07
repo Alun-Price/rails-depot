3c51691e7093b2e26a909e24fb501307a877971f
Alun Price <alunprice089@gmail.com>
2023-08-07 17:41:04 +0100

    G1 Capturing an Order g scaffold Order; creating the Order form; capture the details; update tests; check the db via rails dbconsole

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  #vNeeded to add this for Tailwind CSS to function locally
+  config.assets.debug = true
+
   # Do not eager load code on boot.
   config.eager_load = false
 
